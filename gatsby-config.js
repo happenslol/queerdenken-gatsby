64f89a614dc40d3f34073f2b4efa9cf3cf35f785
Hilmar Wiegand <me@hwgnd.de>
2020-02-11 21:20:46 +0100

    Fix prismic preview yet again

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,47 +2,61 @@ const path = require(`path`)
 require("dotenv").config({ path: `.env` })
 
 module.exports = {
-    plugins: [
-        {
-            resolve: `gatsby-plugin-sass`,
-            options: {
-                cssLoaderOptions: {
-                    camelCase: false,
-                },
-            },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        cssLoaderOptions: {
+          camelCase: false,
         },
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `images`,
-                path: path.join(__dirname, `src`, `images`),
-            },
-        },
-        {
-            resolve: `gatsby-source-prismic-graphql`,
-            options: {
-                repositoryName: "queerdenken",
-                accessToken: `${process.env.PRISMIC_API_KEY}`,
-                previews: true,
-            },
-        },
-        {
-            resolve: `gatsby-plugin-styled-components`,
-            options: {},
-        },
-        {
-          resolve: `gatsby-plugin-manifest`,
-          options: {
-            name: `Queerdenken`,
-            short_name: `Queerdenken`,
-            start_url: `/`,
-            background_color: `#FFFFFF`,
-            theme_color: `#FFFFFF`,
-            display: `standalone`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: path.join(__dirname, `src`, `images`),
+      },
+    },
+    {
+      resolve: `gatsby-source-prismic-graphql`,
+      options: {
+        repositoryName: "queerdenken",
+        accessToken: `${process.env.PRISMIC_API_KEY}`,
+        previews: true,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {},
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Queerdenken`,
+        short_name: `Queerdenken`,
+        start_url: `/`,
+        background_color: `#FFFFFF`,
+        theme_color: `#FFFFFF`,
+        display: `standalone`,
+        pages: [
+          {
+            type: "Home",
+            match: "/",
+            path: "pages",
+            component: require.resolve("./src/templates/index.jsx"),
           },
-        },
-        `gatsby-plugin-sharp`,
-        `gatsby-transformer-sharp`,
-        `gatsby-plugin-react-helmet`,
-    ],
+          {
+            type: "Page",
+            match: "/:uid",
+            path: "pages",
+            component: require.resolve("./src/templates/page.jsx"),
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-react-helmet`,
+  ],
 }
